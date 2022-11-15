720cd9a802dba80e3338cd48a988c873047afdfd
Nick <nicholas.gibbins@blueyonder.co.uk>
2022-11-15 17:57:12 +0000

    Adds  get_winner function to determine who wins and a play function for game loop

--- manual_rps.py
+++ manual_rps.py
@@ -5,21 +5,16 @@ import pygame
 # Define colours and screen
 X_MAX = 1000
 Y_MAX = 1000
-
-black = 0, 0, 0
-white = 255, 255, 255
-red = 255, 0, 0
-green = 0, 255, 0
-blue = 0, 0, 255
+SCORE_DICT = {"R":{"R":0,"P":-1,"S":1}, "P":{"R":1,"P":0,"S":-1}, "S":{"R":-1,"P":1,"S":0}}
+RESULT_MESSAGE_DICT = {-1:"You lost.", 0:"It is a tie!", 1:"You won!"}
 
 size = (X_MAX, Y_MAX)
 screen = pygame.display.set_mode(size)
 
-
 pygame.init()
 
 def get_computer_choice():
-    return random.choice("Rock", "Paper", "Scissors")
+    return random.choice(["R", "P", "S"])
 
 def get_user_choice():
     print("Please make your choice from R for Rock, P for Paper, and S for Scissors")
@@ -29,10 +24,18 @@ def get_user_choice():
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
-                    return "Rock"
+                    return "R"
                 if event.key == pygame.K_p:
-                    return "Paper"
+                    return "P"
                 if event.key == pygame.K_s:
-                    return "Scissors"
+                    return "S"
 
-print(get_user_choice())
+def get_winner(user_choice, computer_choice):
+    print(f"You played {user_choice}, Computer played {computer_choice}")
+    return RESULT_MESSAGE_DICT[SCORE_DICT[user_choice][computer_choice]]
+
+def play():
+    while True:
+        print(get_winner(get_user_choice(), get_computer_choice()))
+
+play()
